0b011650aac77da34f500d83099413c6f2abe0f9
ju4700 <jalal.dev.design@gmail.com>
2025-04-09 20:30:05 +0600

    first commit

---
+++ webbboot-web/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BootForm from "./components/BootForm";
+
+function App() {
+  return (
+    <div className="app-container">
+      <BootForm />
+    </div>
+  );
+}
+
+export default App;
