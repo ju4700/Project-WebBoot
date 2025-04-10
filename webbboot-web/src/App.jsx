a9f6e004317fe1c752c37382779f3d0cbf09e9be
ju4700 <jalal.dev.design@gmail.com>
2025-04-10 19:02:59 +0600

    feat: update index.html and App.jsx to enhance user instructions and improve layout styles

--- webbboot-web/src/App.jsx
+++ webbboot-web/src/App.jsx
@@ -1,10 +1,23 @@
-import React from "react";
-import BootForm from "./components/BootForm";
+import React from 'react';
+import './styles.css';
+import BootForm from './components/BootForm';
 
 function App() {
   return (
-    <div className="app-container">
+    <div className="App">
       <BootForm />
+      <section>
+        <h2>Get WebBoot Companion</h2>
+        <p>To use WebBoot, download and run the companion app for your platform:</p>
+        <ul>
+          <li><a href="/downloads/webbboot-companion-linux.AppImage" download>Linux (.deb)</a></li>
+          <li><a href="/downloads/webbboot-companion-linux.AppImage" download>Linux (.rpm)</a></li>
+          <li><a href="/downloads/webbboot-companion-windows.exe" download>Windows (.exe)</a></li>
+          <li><a href="/downloads/webbboot-companion-macos.dmg" download>macOS (.dmg)</a></li>
+        </ul>
+        <p>After downloading, run the app, then refresh this page to start.</p>
+        <p>Made by <a href='https://github.com/ju4700'>ju4700</a></p>
+      </section>
     </div>
   );
 }
